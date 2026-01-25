a7f0c23b31f45511aaf9ec01e2f39c998fed5d59
İlhan Can Batmaz <ilhancanbatmaz1@gmail.com>
2026-01-25 15:51:37 +0300

    fix: remove deleted ErrorBoundary import from entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async';
 import './index.css'
 import App from './App.tsx'
-import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </ErrorBoundary>
+    <App />
   </StrictMode>,
 )
